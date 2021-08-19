ea89132d3471a58e672dd46c006414bb32b48b2f
Naman Verma <81072434+naman-verma-01@users.noreply.github.com>
2021-08-19 11:33:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#myInput');
+const togglePassword2 = document.querySelector('#togglePassword2');
+const password2 = document.querySelector('#myInput2');
+
+
+togglePassword.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // toggle the eye / eye slash icon
+    this.classList.toggle('bi-eye');
+});
+
+togglePassword2.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = password2.getAttribute('type') === 'password' ? 'text' : 'password';
+    password2.setAttribute('type', type);
+    // toggle the eye / eye slash icon
+    this.classList.toggle('bi-eye');
+});
